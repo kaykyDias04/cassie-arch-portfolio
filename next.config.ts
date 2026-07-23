ceb58d9959f01ed03b50fb0c82c0c3061ae413ab
Kayky Dias <kaykydiasdi@gmail.com>
2026-07-23 10:22:12 -0300

    fix: move serverActions into experimental in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['pg'], // just in case for prisma/pg
-  serverActions: {
-    bodySizeLimit: '10mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
   },
   images: {
     remotePatterns: [
